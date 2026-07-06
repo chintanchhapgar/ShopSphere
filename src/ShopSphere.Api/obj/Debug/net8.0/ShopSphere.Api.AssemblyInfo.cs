efcb46a52e29d35a866a1a7469468141396458b2
Chintan <chhapgarchintan@yahoo.com>
2026-07-06 12:34:16 +0530

    chore: add foundational NuGet packages

--- src/ShopSphere.Api/obj/Debug/net8.0/ShopSphere.Api.AssemblyInfo.cs
+++ src/ShopSphere.Api/obj/Debug/net8.0/ShopSphere.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShopSphere.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efdcef2743a5858f3c8c407dd1b4bd7f232a7609")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShopSphere.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShopSphere.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
